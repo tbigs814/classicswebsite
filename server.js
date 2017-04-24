db6954273ddf328e5969a6f99904ea17cdd4265c
Bigelow <bigett13@deacnet.wfu.edu>
2017-04-23 22:48:45 -0400

    database functionality

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 //loading necessary modules (express, http)
  var express = require('express'); //define variable express, import express package
  var httpModule = require('http');
+ var bodyParser = require('body-parser');
+ const MongoClient = require('mongodb').MongoClient;
+
+ var postTable = "posts";
+
  //create an express app, express is a function
  var app = express();
  //express will be the server for http object created
  var http = httpModule.Server(app);
  //Tells app that any assets (images, logos, etc.) will be found in a folder called assets
   app.use(express.static('assets'));
+
+  app.use(bodyParser.urlencoded({extended: true}));
+
+  app.set('view engine', 'ejs');
+
 
 //  app.get('/mainpage', (req, res)=>{
 //    res.sendFile(__dirname + '/mainpage.html');
@@ -18,11 +28,7 @@
   });
 
 
-  function portlistener(){
-    console.log('Listening in on localhost' + port);
-  }
-  var port = process.env.PORT || 3000; // || = or
-  http.listen(port, portlistener);
+
 
   // Load the module
   var modelTools = require('./models/dataTools.js');
@@ -41,6 +47,73 @@
      });
    });
 
+  app.get('/managePosts', (req, res)=>{
+    // obtain data from movies into cursor object
+  var cursor = db.collection(postTable).find();
+  // console.log(cursor);  // This has too much info
+  // convert to an array to extract the movie data
+  updateIds();
+  cursor.toArray(function (err, results) {
+    if (err)
+      return console.log(err);
+
+    // Render index.ejs
+    res.render('mainpage.ejs', {posts: results});
+    console.log(results)
+  });
+});
+
+app.post('/addPost', (req, res) => {
+  console.log("got POST request");
+  console.log(req.body);
+  db.collection(postTable).save(req.body, (err, result) => {
+    if (err)
+      return console.log(err);
+    console.log('saved to database');
+    updateIds();
+    res.redirect('/managePosts');
+  });
+});
+
  app.post('/people', (req, res) => {
 
  })
+
+ var db;
+// The ids of current entries in the database are keep in array ids.
+var ids = new Array();
+
+var port = process.env.PORT || 3000; // || = or
+MongoClient.connect('mongodb://gsavini:mlab814@ds023495.mlab.com:23495/classicswebsite',
+(err, database) => {
+if (err)
+return console.log(err);
+
+db = database;
+
+updateIds((result)=>{
+    console.log(result);
+  });
+
+  updateIds((result)=>{
+      console.log(result);
+    });
+
+http.listen( port, function () {
+  console.log('Listening on localhost ' + port);
+});
+});
+
+function updateIds(callback) {
+var cursor = db.collection(postTable).find();
+cursor.toArray(function (err, results) {
+  if (err)
+  return console.log(err);
+  ids = [];
+  for (var i = 0; i < results.length; i++) {
+    ids.push(results[i]._id);
+  }
+  if (typeof callback != 'undefined')
+    callback(ids);
+});
+}
